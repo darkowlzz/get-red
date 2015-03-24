ab80e5b74e598f5df6fb88ff1a8fa3c164825934
sunny <indiasuny000@gmail.com>
2015-03-24 13:41:05 +0530

    Cleanup

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //modules and configs
 //===============================================
-var express = require('express');
-var app = express();
+var express = require('express'),
+    router  = express.Router(),
+    app     = express();
 
 var port = process.env.port || 3000; //port set to 3000
 
@@ -9,31 +10,17 @@ var port = process.env.port || 3000; //port set to 3000
 //==================================================
 
 app.use(express.static(__dirname + '/app'));
-app.set('view engine', 'jade');
-
 
 //router configurations
 //============================================
 
-app.get('/', function(req, res){
+router.get('/', function(req, res){
 	res.sendFile('./app/index.html');
-
 });
 
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
-});
-
-app.use(function(error, req, res, next){
-	console.log(error.stack);
-	res.status(500);
-	res.send('500');
-});
-
+app.use('/', router);
 
 //run the server========================
 var server = app.listen(port, function(){
 	console.log('server has started running at localhost:' + port);
 });
-
