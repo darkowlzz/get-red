fbaa044c33bcc0d0177f1d81a63875e7a4b76a9c
sunny <indiasuny000@gmail.com>
2015-03-24 20:45:06 +0530

    Added desktop and mobile breakpoint.

--- app/js/getblood.js
+++ app/js/getblood.js
@@ -1,11 +1,21 @@
 var getBlood = angular.module('getBlood', ['ngMaterial', 'ngMdIcons']);
 
-getBlood.controller('GetBloodCtrl', function($scope) {
-  $scope.description = 'This is the description part of the page. ' +
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat interdum felis sed interdum. Pellentesque ultrices est eget placerat tempor. Curabitur iaculis porttitor mauris. Sed dictum mattis est, sit amet tempor leo placerat id.';
+getBlood.controller('GetBloodCtrl', ['$scope', '$mdMedia',
+  function($scope, $mdMedia) {
+    $scope.description = 'This is the description part of the page. ' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat interdum felis sed interdum. Pellentesque ultrices est eget placerat tempor. Curabitur iaculis porttitor mauris. Sed dictum mattis est, sit amet tempor leo placerat id.';
 
-  $scope.user = {};
-});
+    $scope.user = {};
+    $scope.isMobile = false;
+    $scope.isDesktop = false;
+
+    if ($mdMedia('gt-md')) {
+      $scope.isDesktop = true;
+    } else {
+      $scope.isMobile = true;
+    }
+  }
+]);
 
 getBlood.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
