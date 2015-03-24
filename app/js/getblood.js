55becb603e8bb45c9687e99e96d611b807794209
sunny <indiasuny000@gmail.com>
2015-03-24 17:26:46 +0530

    form ready

--- app/js/getblood.js
+++ app/js/getblood.js
@@ -1,1 +1,14 @@
 var getBlood = angular.module('getBlood', ['ngMaterial', 'ngMdIcons']);
+
+getBlood.controller('GetBloodCtrl', function($scope) {
+  $scope.description = 'This is the description part of the page. ' +
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat interdum felis sed interdum. Pellentesque ultrices est eget placerat tempor. Curabitur iaculis porttitor mauris. Sed dictum mattis est, sit amet tempor leo placerat id.';
+
+  $scope.user = {};
+});
+
+getBlood.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('red')
+    .accentPalette('blue');
+});
