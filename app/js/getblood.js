4b3d79230171b631fcd5a284d643dc4dd82213e0
sunny <indiasuny000@gmail.com>
2015-03-25 21:09:30 +0530

    about page added.

--- app/js/getblood.js
+++ app/js/getblood.js
@@ -5,17 +5,24 @@ var getBlood = angular.module('getBlood', [
 getBlood.controller('MainCtrl', [
   '$scope', '$route', '$routeParams', '$location', '$mdMedia',
   function ($scope, $route, $routeParams, $location, $mdMedia) {
-  
+    $scope.go = function (path) {
+      $location.url(path);
+    };
   }
 ]);
 
+
 getBlood.controller('GetBloodCtrl', [
-  '$scope', '$routeParams',
-  function ($scope, $routeParams) {
+  '$scope', '$routeParams', '$timeout',
+  function ($scope, $routeParams, $timeout) {
     $scope.description = 'This is the description part of the page. ' +
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat interdum felis sed interdum. Pellentesque ultrices est eget placerat tempor. Curabitur iaculis porttitor mauris. Sed dictum mattis est, sit amet tempor leo placerat id.';
 
     $scope.user = {};
+
+    $scope.loading = false;
+    $scope.submitted = false;
+    $scope.error = false;
 
     /*
     $scope.isMobile = false;
@@ -30,15 +37,44 @@ getBlood.controller('GetBloodCtrl', [
 
     $scope.submit = function () {
       console.log('submitting', $scope.user);
-      /*
-      $http.post()
-        .success(function () {
+      var result = validate($scope.user);
+      console.log('result', result);
 
-        });
-      */
+      if (result) {
+        $scope.error = false;
+        $scope.loading = true;
+
+        $timeout(function () {
+          console.log('timeout over!');
+          $scope.submitted = true;
+        }, 4000);
+        /*
+        $http.post()
+          .success(function () {
+            $scope.submitted = true;
+          });
+        */
+      } else {
+        $scope.error = true;
+      }
+    }
+
+    function validate(data) {
+      console.log('validating', data);
+      var result = true;
+
+      // FIXME: Perform better validation
+      if (_.isEmpty(data.name) && _.isEmpty(data.phone) &&
+          _.isEmpty(data.address) && _.isEmpty(data.group) &&
+          _.isEmpty(data.quantity) && _.isEmpty(data.date)) {
+        result = false;
+      }
+
+      return result;
     }
   }
 ]);
+
 
 getBlood.controller('AboutCtrl', [
   '$scope', '$routeParams',
@@ -47,18 +83,23 @@ getBlood.controller('AboutCtrl', [
   }
 ]);
 
-getBlood.config(function($mdThemingProvider, $routeProvider, $locationProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('red')
-    .accentPalette('blue');
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'form.html',
-      controller: 'GetBloodCtrl'
-    })
-    .when('/about', {
-      templateUrl: 'about.html',
-      contrller: 'AboutCtrl'
-    });
-});
+getBlood.config(['$mdThemingProvider', '$routeProvider',
+  function($mdThemingProvider, $routeProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('red')
+      .accentPalette('blue');
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'form.html',
+        controller: 'GetBloodCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+}]);
